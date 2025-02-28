090174253460d757f342a96bc68e3762f61d5171
danielstoyanovv <daniel.stoyanovv@gmail.com>
2025-02-28 07:34:09 +0200

    Update .travis.yml - stop deploy

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=stoyanov1234/fib-server:$SHA
+kubectl set image deployments/client-deployment client=stoyanov1234/fib-client:$SHA
