6069c03c772c56e4c482dd4fbac9f0bd416d0dde
danielstoyanovv <daniel.stoyanovv@gmail.com>
2025-03-01 14:21:37 +0200

    Update .travis.yml - add after success block update deploy.sh - add console log command

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+console.log("Cluster initialization")
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=stoyanov1234/fib-server:$SHA
 kubectl set image deployments/client-deployment client=stoyanov1234/fib-client:$SHA
