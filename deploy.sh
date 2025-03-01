16a6171c66f0fd2b6beda81a6336c5506d4ea387
danielstoyanovv <daniel.stoyanovv@gmail.com>
2025-03-01 15:19:02 +0200

    Update .travis.yml - enable debug mode Update deploy.sh - remove log command

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-console.log("Cluster initialization")
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=stoyanov1234/fib-server:$SHA
 kubectl set image deployments/client-deployment client=stoyanov1234/fib-client:$SHA
